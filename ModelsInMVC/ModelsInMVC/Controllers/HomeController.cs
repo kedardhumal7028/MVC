4f101bf3fe152b68e8c3f34ff9e819de1a2e7b37
Kedar Dhumal <kedardhumal31@gmail.com>
2022-07-03 22:21:41 +0530

    MVC

---
+++ ModelsInMVC/ModelsInMVC/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ModelsInMVC.Models;
+
+namespace ModelsInMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = GetEmployee();
+            return View(data);
+        }
+
+
+        private Employee GetEmployee()
+        {
+            return new Employee()
+            {
+                ID = 1,
+                Name = "Kedar",
+                Address = "Karad"
+            };
+        }
+    }
+}
---
+++ Practice/ModelsInMvcPractice2/ModelsInMvcPractice2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ModelsInMvcPractice2.MvcApplication" Language="C#" %>
