911487c2b5b752761e69aea5fcd721db833e220b
Kedar Dhumal <kedardhumal31@gmail.com>
2022-07-06 01:09:13 +0530

    Directives in Razor

---
+++ RazorView_Loop_Conditional_Statement/RazorView_Loop_Conditional_Statement/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RazorView_Loop_Conditional_Statement.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
