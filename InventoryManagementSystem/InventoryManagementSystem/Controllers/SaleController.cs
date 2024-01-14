cb3a84942d4f00a189234e7f58c569edb388f0f9
Kedar Dhumal <kedardhumal31@gmail.com>
2024-01-14 21:49:15 +0530

    InventoryMgtSystem

---
+++ InventoryManagementSystem/InventoryManagementSystem/Controllers/SaleController.cs
@@ -0,0 +1,20 @@
+﻿using InventoryManagementSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InventoryManagementSystem.Controllers
+{
+    public class SaleController : Controller
+    {
+        Inventory_Management_SystemEntities db = new Inventory_Management_SystemEntities();
+
+        // GET: Sale
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
