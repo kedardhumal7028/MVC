2fda3be79c6bd643eb45f6c1070864d92cc8734d
Kedar Dhumal <kedardhumal31@gmail.com>
2024-02-25 22:35:31 +0530

    MVC

---
+++ Assignments  and Practice/ViewBagAndViewData/ViewBagAndViewData/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ViewBagAndViewData.Models;
+
+namespace ViewBagAndViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Hi Keds... Welcome to Home Index.";
+            ViewBag.MyList = new List<string>() {"Kedar","Suraj","Chaitya","Abhi" };
+
+            List<Employee> emplist = new List<Employee>()
+            {
+                new Employee(){Email = "keds@gmail.com", Id = 1, Name = "Keds" },
+                new Employee() {Email = "Suraj@gmail.com", Id = 2, Name = "Suraj" },
+                new Employee(){Email = "Chaitya@gmail.com", Id = 3, Name = "Chaitya"},
+                new Employee(){Email = "Abhi@gmail.com", Id = 4, Name = "Abhi" }
+            };
+            ViewBag.Myemplist = emplist;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Hi Keds...Welcome to Home About.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Hi Keds... Welcome to Home Contact.";
+
+            return View();
+        }
+    }
+}
--- Projects/InventoryManagementSystem/InventoryManagementSystem/SaleReport.aspx
+++ Projects/InventoryManagementSystem/InventoryManagementSystem/SaleReport.aspx
@@ -1,5 +1,7 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="SaleReport.aspx.cs" Inherits="InventoryManagementSystem.SaleReport" %>
 
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=10.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -8,8 +10,11 @@
 </head>
 <body>
     <form id="form1" runat="server">
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
         <div>
+            <rsweb:reportviewer runat="server"></rsweb:reportviewer>
         </div>
     </form>
 </body>
+
 </html>
